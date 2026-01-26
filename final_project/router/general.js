2a2af1980dd287bf3d52d50b423bccae862cdca0
chirag-bajaj-creator <chiragbajaj2203@gmail.com>
2026-01-26 09:32:08 +0530

    intial changes in gen.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,26 +13,70 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json({
+    books:books,
+    message: "all books retrieved"
+  });
 });
-
+  
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn=req.params.isbn;
+  let isbn_extrcat=books[isbn];
+  if(!isbn_extrcat){
+    return res.status(404).json({message: "ISBN not found"});
+  }
+  else{
+  return res.status(200).json({
+    books:isbn_extrcat,
+    message: "isbn extracted successfully"
+  });
+}
+
  });
   
-// Get book details based on author
+// & THE GET BOOKS BASED ON AUTHOR & //
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// * THIS IS THE SIMPLE CODE FOR THE AUTHOR SEARCH BUT IT WILL NOT WORK AS EXPECTED AS WE NEED TO LOOP THROUGH THE BOOKS OBJECT TO FIND THE AUTHOR *
+// TODO: To search the books by converting author name to the lowercase 
+let author=req.params.author.toLowerCase();
+const result=Object.values(books).filter(books=>books.author.toLowerCase()===author);
+if(result.length>0){
+  return res.status(200).json({
+    message:'books Details by author found successfully',
+    books:result
+   
+});
+}
+else{
+  return res.status(404).json({'message':'No Book Found By The Author Name',
+  books:[]
+});
+}
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+  let title=req.params.title.toLowerCase();
+  const result_title=Object.values(books).filter(books=>books.title.toLowerCase()===title);
+  if(result_title.length>0){
+    return res.status(200).json({
+      books:result_title,
+      message:'Books details by title found successfully'
+    });
+  }
+   else{
+    return res.status(404).json({
+      message:'No Books Found by Title',
+      books:[]
+    });
+   }
+  });
+
+
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
